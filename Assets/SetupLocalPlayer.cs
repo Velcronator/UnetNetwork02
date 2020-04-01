f919579be03f4a0d6de69f790f21d9dbb50b17f5
Velcronator <valacos7@gmail.com>
2020-04-02 00:13:01 +1100

    L14 Added Player Name Label Prefab above the car

--- Assets/SetupLocalPlayer.cs
+++ Assets/SetupLocalPlayer.cs
@@ -1,8 +1,13 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class SetupLocalPlayer : NetworkBehaviour {
+
+    public Text namePlayerPrefab;   // Prefab
+    public Text nameLabel;          // Instants of the prefab
+    public Transform namePos;
 
 	// Use this for initialization
 	void Start () 
@@ -16,11 +21,16 @@ public class SetupLocalPlayer : NetworkBehaviour {
 		{
 			GetComponent<PlayerController>().enabled = false;
 		}
+
+        GameObject canvas = GameObject.FindWithTag("MainCanvas");
+        nameLabel = Instantiate(namePlayerPrefab, Vector3.zero, Quaternion.identity) as Text;
+        nameLabel.transform.SetParent(canvas.transform);
 	}
 
 	void Update()
 	{
-
+        Vector3 nameLabelPos = Camera.main.WorldToScreenPoint(namePos.position);
+        nameLabel.transform.position = nameLabelPos;
 	}
 
 }
